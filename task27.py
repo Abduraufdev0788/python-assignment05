14a986301e651f9fd042c24bd96e34ac3b352414
abduraufdev0788 <abduraufnasrullayev790@gmail.com>
2025-05-22 23:05:26 +0500

    ajoyib misol lekin ozgaruvchini yangilasam xato chiqadi ekan shuning uchun  ozgina kopayib ketti

--- task27.py
+++ task27.py
@@ -1,3 +1,11 @@
-a=38500
-
-    
+sum = int(input("pulni kiriting: "))
+on = sum //10000
+b =sum-(on* 10000)
+c = b //5000
+d = b - (c * 5000)
+e = d //1000
+f = d - (e * 1000)
+g = f //500
+h = f - (g * 500)
+i = h //100
+print(f"pulingizda {on} ta 10000 so'm, {c} ta 5000 so'm, {e} ta 1000 so'm, {g} ta 500 so'm, {i} ta 100 so'm bor")
